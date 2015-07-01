74444664ca64a683e29e6ad223716cd2172f0ecb
tarena234 <tarena234@tarena234deiMac.local>
2015-07-01 11:05:22 +0800

    add eye

--- demo01-Git-Local/ViewController.m
+++ demo01-Git-Local/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"第一次.......");
     NSLog(@"fffffffffff");
+    NSLog(@"yyyyyyyyyy");
 }
 
 - (void)didReceiveMemoryWarning {
