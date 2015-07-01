6d2a685e6e45897cc605f60733d807a3954e8697
tarena234 <tarena234@tarena234deiMac.local>
2015-07-01 09:38:31 +0800

    Initial Commit

---
+++ demo01-Git-LocalTests/demo01_Git_LocalTests.m
@@ -0,0 +1,40 @@
+//
+//  demo01_Git_LocalTests.m
+//  demo01-Git-LocalTests
+//
+//  Created by tarena234 on 15/7/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface demo01_Git_LocalTests : XCTestCase
+
+@end
+
+@implementation demo01_Git_LocalTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
